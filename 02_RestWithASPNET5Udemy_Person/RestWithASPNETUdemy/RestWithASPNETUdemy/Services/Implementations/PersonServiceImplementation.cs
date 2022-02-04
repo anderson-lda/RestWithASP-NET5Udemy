686a8e828ebc43caa5cb81aedb6e6e0cf45ed4c6
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-02-03 22:23:50 -0300

    created connection with mysql

--- 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -1,12 +1,19 @@
 ﻿using RestWithASPNETUdemy.Model;
+using RestWithASPNETUdemy.Model.Context;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace RestWithASPNETUdemy.Services.Implementations
 {
     public class PersonServiceImplementation : IPersonService
     {
-        private volatile int count;
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
 
         public Person Create(Person person)
         {
@@ -20,20 +27,14 @@ namespace RestWithASPNETUdemy.Services.Implementations
 
         public List<Person> FindAll()
         {
-            List<Person> people = new List<Person>();
-            for(int i = 0; i < 10; i++)
-            {
-                Person person = MockPerson(i);
-                people.Add(person);
-            }
-            return people;
+            return _context.Persons.ToList();
         }
 
         public Person FindById(long id)
         {
             return new Person
             {
-                Id = IncrementAndGet(),
+                Id = 1,
                 FirstName = "Anderson",
                 LastName = "Silva",
                 Address = "ES - BR",
@@ -46,21 +47,5 @@ namespace RestWithASPNETUdemy.Services.Implementations
             return person;
         }
 
-        private Person MockPerson(int i)
-        {
-            return new Person
-            {
-                Id = IncrementAndGet(),
-                FirstName = "Person Name "+i,
-                LastName = "Person Last Name "+i,
-                Address = "SP - BR",
-                Gender = "Male"
-            };
-        }
-
-        private long IncrementAndGet()
-        {
-            return Interlocked.Increment(ref count);
-        }
     }
 }
