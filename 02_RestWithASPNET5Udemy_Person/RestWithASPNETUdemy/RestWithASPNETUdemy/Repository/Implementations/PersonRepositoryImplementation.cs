0445469400a2f0568a53d2bb292e2fecb9c0f417
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-02-05 14:19:18 -0300

    adjusted update method from repository

--- 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
+++ 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Repository/Implementations/PersonRepositoryImplementation.cs
@@ -54,7 +54,7 @@ namespace RestWithASPNETUdemy.Repository.Implementations
 
         public Person Update(Person person)
         {
-            if (!Exists(person.Id)) return new Person();
+            if (!Exists(person.Id)) return null;
             var result = _context.Persons.SingleOrDefault(p => p.Id == person.Id);
             if (result != null)
             {
