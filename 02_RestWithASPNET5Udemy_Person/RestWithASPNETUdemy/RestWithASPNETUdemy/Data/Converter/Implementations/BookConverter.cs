69aef3f61de9d497d187c9243b27fedd3d22bcde
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-02-11 11:53:55 -0300

    added parse for book

---
+++ 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,53 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Contract;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Data.Converter.Implementations
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+
+            return new Book
+            {
+                Id = origin.Id,
+                author = origin.author,
+                launch_date = origin.launch_date,
+                price = origin.price,
+                title = origin.title,
+            };
+        }
+
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+
+            return new BookVO
+            {
+                Id = origin.Id,
+                author = origin.author,
+                launch_date = origin.launch_date,
+                price = origin.price,
+                title = origin.title,
+            };
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
---
+++ 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/BookVO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class BookVO
+    {
+        public long Id { get; set; }
+        public string author { get; set; }
+        public DateTime launch_date { get; set; }
+        public decimal price { get; set; }
+        public string title { get; set; }
+    }
+}
