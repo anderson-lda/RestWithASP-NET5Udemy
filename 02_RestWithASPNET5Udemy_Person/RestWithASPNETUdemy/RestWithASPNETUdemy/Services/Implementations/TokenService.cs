1b79e290f8fffbc8ce655c2f1afdda7f75337e4e
Anderson Lima <anderson.lima.araujo@hotmail.com>
2022-02-19 19:58:13 -0300

    adjusted some points

--- 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/TokenService.cs
+++ 02_RestWithASPNET5Udemy_Person/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/TokenService.cs
@@ -57,7 +57,7 @@ namespace RestWithASPNETUdemy.Services.Implementations
 
             ClaimsPrincipal principal = tokenHandler.ValidateToken(token,tokenValidationParameters,out securityToken);
             var jwtSecurityToken = securityToken as JwtSecurityToken;
-            if (jwtSecurityToken != null ||
+            if (jwtSecurityToken == null ||
                 !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256,
                 StringComparison.InvariantCulture)) throw new SecurityTokenException("Invalid token");
             return principal;
